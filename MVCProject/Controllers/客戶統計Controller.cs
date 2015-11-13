8d25992b4113c8c6f010d118caaf287ac434b982
BarryPai <ruichungbai@gmail.com>
2015-11-13 23:58:05 +0800

    MVC5 course HW1

---
+++ MVCProject/Controllers/客戶統計Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class 客戶統計Controller : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: 客戶統計
+        public ActionResult Index()
+        {
+            return View(db.客戶統計.ToList());
+        }
+
+        // GET: 客戶統計/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶統計 客戶統計 = db.客戶統計.Find(id);
+            if (客戶統計 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶統計);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCProject/Models/MobileValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class MobileValidation : DataTypeAttribute
+    {
+        public MobileValidation() : base(DataType.Text)
+        {
+            this.ErrorMessage = "Mobile fields error";
+        }
+
+        public override bool IsValid(object value)
+        {
+            string pattern = @"^\d{4}-\d{6}$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(Convert.ToString(value));
+        }
+    }
+}
